1715239305b4ac6aa536f261488f4f2924cab701
Alistair Wallace <apsw@wallace2.com>
2018-05-15 16:32:42 +0100

    Adding vibration sensor code and data for Ian

---
+++ sa-vibration-sensor/sa-vibration-sensor.ino
@@ -0,0 +1,14 @@
+const int PIEZO_PIN = A0; // Piezo output
+
+void setup() 
+{
+  Serial.begin(9600);
+}
+
+void loop() 
+{
+  // Read Piezo ADC value in, and convert it to a voltage
+  int piezoADC = analogRead(PIEZO_PIN);
+  float piezoV = piezoADC / 1023.0 * 100;
+  Serial.println(piezoV); // Print the voltage.
+}
