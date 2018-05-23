2b8464d9cb4b66508d70a606ffecaacc7bdc96fd
Alistair Wallace <apsw@wallace2.com>
2018-05-23 17:22:34 +0100

    End of day (23/05/18)

---
+++ aw-ml-algorithm/online_examples/aiml/recurrent_nn_tutorial/rnn_ex.py
@@ -0,0 +1,34 @@
+# Recurrent Neural Network example
+#http://adventuresinmachinelearning.com/recurrent-neural-networks-lstm-tutorial-tensorflow/ 
+
+import tensorflow as tf
+import numpy as np 
+import collections
+import os 
+import argparse
+import datetime as dt 
+
+
+def load_data():
+    # get the data paths
+    train_path = os.path.join(data_path, "ptb.train.txt")
+    valid_path = os.path.join(data_path, "ptb.valid.txt")
+    test_path = os.path.join(data_path, "ptb.test.txt")
+
+    # Build the complete vocabulary, then convert the 
+    # test data to a list of integers
+    word_to_id = build_vocab(train_path)
+    train_data = file_to_word_ids(train_path, word_to_id)
+    valid_data = file_to_word_ids(valid_path, word_to_id)
+    test_data = file_to_word_ids(test_path, word_to_id)
+    vocabulary = len(word_to_id)
+    reversed_dictionary = dict(
+                            zip(
+                                word_to_id.values(),
+                                word_to_id.keys()))
+
+    print(train_data[:5])
+    print(word_to_id)
+    print(vocabulary)
+    print(" ".join([reversed_dictionary[x] for x in train_data[:10]]))
+    return train_data, valid_data, test_data, vocabulary, reversed_dictionary
